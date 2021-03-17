3314bf5f17c12490f88efc386c0bba1148d20e05
Aleksei <ponibrashin@yandex.ru>
2021-03-17 23:42:06 +0700

    Сделана авторизация

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { cookieMiddleware } = require('./middleware/auth')
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin');
 
 var app = express();
-
+cookieMiddleware(app)
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -20,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ middleware/auth.js
@@ -0,0 +1,42 @@
+function cookieMiddleware(app) {
+  const express = require('express')
+  const cookieParser = require('cookie-parser')
+  const session = require('express-session')
+  const FileStore = require('session-file-store')(session)
+  const mongoose = require("mongoose");
+
+  app.use(cookieParser())
+  app.use(
+    session({
+      store: new FileStore(),
+      key: "user_sid",
+      secret: "Animals",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        expires: 6000000
+      }
+    })
+  )
+
+  function cookiesCleaner(req, res, next) {
+    if (req.cookies.user_sid && !req.session.user) {
+      res.clearCookie("user_sid");
+    }
+    next();
+  }
+  app.use(cookiesCleaner)
+}
+
+const sessionChecker = (req, res, next) => {
+  if (!req.session.user) {
+    res.redirect("/animals");
+  } else {
+    next();
+  }
+};
+
+module.exports = {
+  sessionChecker,
+  cookieMiddleware
+};
