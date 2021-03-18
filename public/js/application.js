4eb1f5fe43ee9ffc12345d941b497fa6f07d520b
DmitriyP <diman6374@yandex.ru>
2021-03-18 22:22:38 +0300

    func krome tarifov

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,101 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.carousel');
-  var instances = M.Carousel.init(elems, {fullWidth: true});
+
+  const prices = document.querySelector('#price')
+  prices.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const lastPrice = document.getElementsByClassName("cost")
+    console.log(lastPrice);
+    // const response = fetch()
+  })
+
+  const elems = document.querySelectorAll('.carousel');
+  const instances = M.Carousel.init(elems, {fullWidth: true});
+  let a = document.getElementById('aName')
+  if (a) {
+    function nameClick() {
+      const animalName = document.getElementsByClassName('animalName')[0]
+      animalName.addEventListener('click', async (e) => {
+        e.preventDefault()
+        let h = document.getElementById('aName')
+        let prev = e.target.parentNode
+        console.log(prev);
+        const res = await fetch('http://localhost:3000/views/inputAnimal.hbs')
+        console.log(res);
+        const source = await res.text()
+        console.log(source);
+        const template = Handlebars.compile(source)
+        console.log(template);
+        const html = template({val:prev.textContent, id:h.id})
+        console.log(html);
+        e.target.parentNode.innerHTML = html
+        hnew = document.getElementById('aName')
+        hnew.addEventListener('submit', async (el) => {
+          el.preventDefault()
+          let parent = document.getElementById('aName')
+          let insert = document.getElementsByClassName('animalN')[0]
+          console.log(insert);
+          console.log(insert.id);
+          let response = await fetch(`/animal/${insert.id}`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              name:parent.animalN.value,
+            }) 
+          })
+          let res = await response.json()
+          alert(res)
+          insert.innerHTML = getBackName(parent.animalN.value)
+          nameClick()
+        })
+      })
+    }
+    nameClick()
+  
+    function contentClick() {
+      const animalContent = document.getElementsByClassName('animalContent')[0]
+      animalContent.addEventListener('click', async (e) => {
+        e.preventDefault()
+        let h = document.getElementById('aContent')
+        let prev = e.target.parentNode
+        const res = await fetch('http://localhost:3000/views/inputAnimal.hbs')
+        const source = await res.text()
+        const template = Handlebars.compile(source)
+        const html = template({val:prev.textContent, id:h.id})
+        e.target.parentNode.innerHTML = html
+        hnew = document.getElementById('aContent')
+        hnew.addEventListener('submit',async (el) => {
+          el.preventDefault()
+          let parent = document.getElementById('aContent')
+          let insert = document.getElementsByClassName('animalC')[0]
+          let getId = document.getElementsByClassName('animalN')[0]
+          // console.log(insert.id);
+          let response = await fetch(`/animal/${getId.id}/content`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              content:parent.animalN.value,
+            }) 
+          })
+          let res = await response.json()
+          insert.innerHTML = getBackContent(parent.animalN.value)
+          alert(res)
+          contentClick()
+        })
+      })
+    }
+    contentClick()
+  
+    function getBackName (val) {
+      return `${val} <img id ="aName" class = "redact animalName" src="/images/redact.png">`
+    }
+    function getBackContent (val) {
+      return `${val} <img id ="aContent" class = "redact animalContent" src="/images/redact.png">`
+    }
+
+  }
 });
+
--- routes/animal.js
+++ routes/animal.js
@@ -14,11 +14,27 @@ const storage = multer.diskStorage({
 })
 const upload = multer({ storage:storage })
 
-router.get('/', async (req, res) => {
+router.get('/',adminChecker, async (req, res) => {
   let animals = await Animal.find()
   res.render('animal', {animals});
 });
-
+router.get('/new', adminChecker, adminChecker,(req, res) => {
+  // console.log("hghghjhghg");
+  res.render('new')
+})
+router.post('/',adminChecker, upload.single('filedata'), async (req,res) => {
+  const {name, info} = req.body
+  console.log(name,info);
+  let path = `/images/${req.file.filename}`
+  console.log();
+  await Animal.create({
+    name,
+    info,
+    img: [path,path,path,path,path],
+    frontImg: path
+  })
+  res.redirect("/animal")
+})
 router.get('/:id', adminChecker, async (req,res) => {
   let animal = await Animal.findOne({_id:req.params.id})
   res.locals.animalPictures = animal.img
@@ -27,11 +43,23 @@ router.get('/:id', adminChecker, async (req,res) => {
   res.locals.animalId = animal._id
   res.render('oneanimal')
 })
-router.post('/:id',upload.single('filedata'), async (req,res) => {
+router.post('/:id', adminChecker,upload.single('filedata'), async (req,res) => {
   let path = `/images/${req.file.filename}`
   let animal = await Animal.findOne({_id:req.params.id})
   animal.img.push(path)
   await animal.save()
   res.redirect(`/animal/${req.params.id}`)
 })
+router.put('/:id/content', adminChecker, async (req,res) => {
+  const {content} = req.body
+  console.log(content, req.params.id);
+  await Animal.findOneAndUpdate({_id:req.params.id},{info:content} )
+  res.json('Обновлено')
+} )
+router.put('/:id', adminChecker, async (req,res) => {
+  const {name} = req.body
+  await Animal.findOneAndUpdate({_id:req.params.id},{name:name} )
+  res.json('Обновлено')
+})
+
 module.exports = router;
