fb4b4077d57dc33ddfdd44e11c14bd38ad16acf5
DmitriyP <diman6374@yandex.ru>
2021-03-18 16:00:12 +0300

    update and carusel

--- models/animal.js
+++ models/animal.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 const animalScheme = new mongoose.Schema({
   name: String,
   info: String,
-  img: []
-
+  img: {
+    type:Array,
+    default:[]
+  },
+  frontImg:String
 });
 
 module.exports = mongoose.model('Animal', animalScheme)
--- routes/animal.js
+++ routes/animal.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const { route } = require('.');
 const router = express.Router();
+const Animal = require('../models/animal');
 
-router.get('/', (req, res) => {
-  res.render('animal');
+router.get('/', async (req, res) => {
+  let animals = await Animal.find()
+  res.render('animal', {animals});
 });
 
+router.get('/:id', async (req,res) => {
+  let animal = await Animal.findOne({_id:req.params.id})
+  res.locals.animalPictures = animal.img
+  res.locals.animalContent = animal.info
+  res.locals.animalName = animal.name
+  res.render('oneanimal')
+})
 
 module.exports = router;
